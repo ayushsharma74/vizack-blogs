ba00d7e816938b6f624abd987ad9dfb3f22a19d3
Gajender <Gajendrasuman868@gmail.com>
2024-04-26 16:42:25 +0530

    Delete Blogs directory

--- Blogs/Name/pending/Blog Title/page.jsx
+++
@@ -1,4 +0,0 @@
-<BlogTitle title={"Blog Title"} writer={"Name"} date={"25.4.2024"} category={"Category"} /> 
-<heading content={'Heading'} />
-<par content={'Paragraph'} />
-<MainImg link={'Image Address'} alt={' Image Alt'} />
