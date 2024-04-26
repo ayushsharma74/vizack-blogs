02374bf92e16c1d0104e1de00def4ce3df12c2d6
Ayush Sharma <134384316+ayushsharma74@users.noreply.github.com>
2024-04-26 22:07:02 +0530

    Create new directory

--- Blogs/Name/pending/Blog Title/page.jsx
+++ Blogs/Name/pending/Blog Title/page.jsx
@@ -2,3 +2,10 @@
 <heading content={'Heading'} />
 <par content={'Paragraph'} />
 <MainImg link={'Image Address'} alt={' Image Alt'} />
+<BlogTitle title={"Blog Title"} writer={"Name"} date={"26.4.2024"} category={"Category"} /> 
+<heading content={'Heading'} />
+<par content={'Paragraph'} />
+<MainImg link={'Image Address'} alt={' Image Alt'} />
+<heading content={'Heading'} />
+<par content={'Paragraph'} />
+<MainImg link={'Image Address'} alt={' Image Alt'} />
