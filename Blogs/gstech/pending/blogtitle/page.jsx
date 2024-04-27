1c1678b03b223cb9e02647c66ea0a4c557a890ec
Ayush Sharma <134384316+ayushsharma74@users.noreply.github.com>
2024-04-27 20:19:03 +0530

    Create new directory

---
+++ Blogs/gstech/pending/blogtitle/page.jsx
@@ -0,0 +1,4 @@
+<BlogTitle title={"Blog Title"} writer={"gstech"} date={"27.4.2024"} category={"Category"} /> 
+<heading content={'Heading'} />
+<par content={'Paragraph'} />
+<MainImg link={'Image Address'} alt={' Image Alt'} />
