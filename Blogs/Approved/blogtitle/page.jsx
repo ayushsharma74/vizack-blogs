235df9a59116abf78284b766965bf3ea151dc1db
Ayush Sharma <134384316+ayushsharma74@users.noreply.github.com>
2024-05-02 20:49:35 +0530

    Move Directory

---
+++ Blogs/Approved/blogtitle/page.jsx
@@ -0,0 +1,24 @@
+
+head-Heading
+
+par-Paragraph
+
+link-Image Address alt- Image Alt size-full
+
+head-Heading
+
+par-Paragraph
+
+link-Image Address alt- Image Alt size-full
+
+head-Heading
+
+par-Paragraph
+
+link-Image Address alt- Image Alt size-full
+
+head-Heading
+
+par-Paragraph
+
+link-Image Address alt- Image Alt size-full
